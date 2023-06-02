5f42814b2280f0ab4bf1613497e7a9cb9de9cefd
Alexandregaudencio <alexandre.gtpjr@gmail.com>
2023-06-01 21:15:43 -0300

    Feat: TimeManager

--- Game1.cs
+++ Game1.cs
@@ -1,5 +1,6 @@
 ﻿using ECS.Core.Components.Cam;
 using ECS.Core.Components.Collision;
+using ECS.Core.Managers;
 using ECS.Core.Object;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
@@ -13,11 +14,19 @@ namespace ECS
         private readonly GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        #region Keep Managers on Game1
         private readonly CollisionManager collisionManager;
+        private readonly TimeManager timeManager;
+        #endregion
+
+
+        #region camera and cameraconfig on Scene
         private readonly Camera camera;
         private readonly CameraController cameraController;
+        #endregion
 
-        #region Objects
+
+        #region Objects - just for test
         private readonly Plane floor;
         //private WindMill windMill;
         //private WindMill windMill2;
@@ -30,16 +39,18 @@ namespace ECS
 
         public Game1()
         {
-
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
 
-            collisionManager = new CollisionManager(this);
 
+            collisionManager = new CollisionManager(this);
+            timeManager =  new TimeManager(this);
+
+            #region Objects Instances of test
             camera = new Camera(this, new Vector3(0, 10, 2));
             cameraController = new CameraController(this, camera);
-
+            
             floor = new Plane(this, camera, Color.DarkBlue);
             floor.Transform.SetScale(Vector3.One * 1000);
             box = new Box(this, camera);
@@ -49,29 +60,32 @@ namespace ECS
             box2.Transform.Translate(Vector3.UnitX * 4);
             box2.MovementControl.Active = true;
 
-
             sphere = new Sphere(this, camera);
             sphere.SetObjectOnFloorY();
             sphere.Transform.Translate(Vector3.UnitZ * 3);
-            //box.Transform.IncreaseScale(Vector3.One * 2);
 
-            //collisionManager.AddColliders(collider, collider1, collider2, collider3);
-            //collisionManager.AddColliders(box.collider);
+            //box.Transform.IncreaseScale(Vector3.One * 2);
             //house = new House(this, camera);
             //windMill = new WindMill(this, camera, new Vector3(0,0,30 ));
+
+            #endregion
+
         }
 
         protected override void Initialize()
         {
+            
+            Components.Add(collisionManager);
+            Components.Add(timeManager);
 
+            #region Add Instances of test
             Components.Add(camera);
             Components.Add(cameraController);
-            Components.Add(collisionManager);
             Components.Add(floor);
             Components.Add(box);
             Components.Add(box2);
             Components.Add(sphere);
-            //Components.Add(collisionManager);
+            #endregion
 
             //Components.Add(house);
             //Components.Add(windMill);
@@ -97,10 +111,8 @@ namespace ECS
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Back))
                 Exit();
 
-            //plane.Transform.RotateY(MathHelper.ToRadians(1)) ;
 
-            //if (Keyboard.GetState().IsKeyDown(Keys.NumPad1)) cameraController.SetCameraTarget(modelRenderer.Transform);
-            if (Keyboard.GetState().IsKeyDown(Keys.NumPad0)) cameraController.SetCameraTarget(box.Transform);
+            if (Keyboard.GetState().IsKeyDown(Keys.NumPad0)) cameraController.SetCameraTarget(sphere.Transform);
             base.Update(gameTime);
 
 
